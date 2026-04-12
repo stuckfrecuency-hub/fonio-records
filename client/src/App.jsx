3fb699a3827249c352e7827e2dcbb6132f5056f9
Fonio Records <stuckfrecuency@gmail.com>
2026-04-12 18:07:01 -0500

    Initial commit - setup project

---
+++ client/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <section id="center">
+        <div className="hero">
+          {/* AQUÍ ESTÁ TU LOGO */}
+          <img 
+            src="/logotype.jpg" 
+            className="base" 
+            width="250" 
+            alt="Fonio Records Logo" 
+          />
+        </div>
+        <div>
+          <h1>Fonio Records</h1>
+          <p>
+            Sello discográfico independiente.
+          </p>
+        </div>
+        <button
+          className="counter"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          Interacciones: {count}
+        </button>
+      </section>
+    </>
+  )
+}
+
+export default App
